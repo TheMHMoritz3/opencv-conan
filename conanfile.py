050449d6825fe5058790c08f700a8da867776e09
Moritz Herzog <moritz.herzog@german-software-engineers.com>
2020-03-05 11:04:56 +0100

    Adding Dependencys

--- conanfile.py
+++ conanfile.py
@@ -3,37 +3,59 @@ import os
 
 class OpencvConan(ConanFile):
     name = "opencv"
-    version = "master"
+    version = "4.2.0"
     license = "<Put the package license here>"
     author = "<Put your name here> <And your email here>"
     url = "<Package recipe repository url here, for issues about the package>"
     description = "<Description of Opencv here>"
     topics = ("<Put some tag here>", "<here>", "<and here>")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False], "NonFree": [True, False], "Contrib": [True, False], "WithVTK": [True, False]}
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "NonFree": [True, False],
+               "Contrib": [True, False],
+               "WithVTK": [True, False]}
     short_paths = True
-    default_options = {"shared": True, "fPIC": True, "NonFree": True, "Contrib": True, "WithVTK": False}
+    default_options = {"shared": True,
+                       "fPIC": True,
+                       "NonFree": True,
+                       "Contrib": True,
+                       "WithVTK": False}
     generators = "cmake"
-    requires = ("eigen/3.3.7@conan/stable", "libpng/1.6.37")
+    requires = ("eigen/3.3.7@conan/stable",
+                "zlib/1.2.11",
+                "libpng/1.6.37",
+                "libjpeg-turbo/1.5.2@bincrafters/stable",
+                "libwebp/1.0.0@bincrafters/stable",
+                "libtiff/4.0.9@bincrafters/stable",
+                "jasper/2.0.14@conan/stable",
+                "protobuf/3.5.2@bincrafters/stable",
+                "gflags/2.2.2@bincrafters/stable",
+                "glog/0.4.0@bincrafters/stable",
+                "bzip2/1.0.8@conan/stable")
 
     def requirements(self):
+        self.options["jasper"].jpegturbo = True
         if self.options.WithVTK:
             self.requires("libvtk/7.1.1@mhmoritz3/stable")
 
 
     def source(self):
-        self.run("git clone https://github.com/opencv/opencv.git")
+        tools.download("https://github.com/opencv/opencv/archive/4.2.0.zip", "opencv.zip")
+        tools.unzip("opencv.zip", ".")
 
         if self.options.Contrib:
-            self.run("git clone https://github.com/opencv/opencv_contrib.git")
+            tools.download("https://github.com/opencv/opencv_contrib/archive/4.2.0.zip", "opencv_contrib.zip")
+            tools.unzip("opencv_contrib.zip", ".")
 
-        tools.replace_in_file("opencv/CMakeLists.txt", "project(OpenCV CXX C)",
+        tools.replace_in_file("opencv-4.2.0/CMakeLists.txt", "project(OpenCV CXX C)",
                               '''project(OpenCV CXX C)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def configure_cmake(self):
         cmake = CMake(self)
+
         cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = True
         cmake.definitions['BUILD_JAVA'] = False
         cmake.definitions['BUILD_opencv_java_bindings_generator'] = False
@@ -44,11 +66,34 @@ conan_basic_setup()''')
         cmake.definitions['BUILD_opencv_python_tests'] = False
         cmake.definitions['OPENCV_ENABLE_NONFREE'] = self.options.NonFree
         cmake.definitions['WITH_GTK'] = False
+        cmake.definitions['BUILD_PNG'] = False
+        cmake.definitions['WITH_PNG'] = True
+        cmake.definitions['BUILD_TBB'] = False
+        cmake.definitions['WITH_TBB'] = False
+        cmake.definitions['BUILD_TIFF'] = False
+        cmake.definitions['WITH_TIFF'] = True
+        cmake.definitions['BUILD_WEBP'] = False
+        cmake.definitions['WITH_WEBP'] = True
+        cmake.definitions['BUILD_JASPER'] = False
+        cmake.definitions['WITH_JASPER'] = True
+        cmake.definitions['BUILD_JPEG'] = False
+        cmake.definitions['WITH_JPEG'] = True
+        cmake.definitions['BUILD_ZLIB'] = False
+        cmake.definitions['BUILD_PROTOBUF'] = False
+        cmake.definitions['PROTOBUF_UPDATE_FILES'] = True
+        cmake.definitions['WITH_PROTOBUF'] = True
+        cmake.definitions['GFLAGS_INCLUDE_DIR_HINTS'] = ';'.join(self.deps_cpp_info['gflags'].include_paths)
+        cmake.definitions['GFLAGS_LIBRARY_DIR_HINTS'] = ';'.join(self.deps_cpp_info['gflags'].lib_paths)
+        cmake.definitions['GLOG_INCLUDE_DIR_HINTS'] = ';'.join(self.deps_cpp_info['glog'].include_paths)
+        cmake.definitions['GLOG_LIBRARY_DIR_HINTS'] = ';'.join(self.deps_cpp_info['glog'].lib_paths)
+        cmake.definitions['BUILD_opencv_dnn'] = False
+        cmake.definitions['BUILD_opencv_dnn_objdetect'] = False
+        cmake.definitions['BUILD_opencv_dnn_superres'] = False
 
         if self.options.Contrib:
-            cmake.definitions['OPENCV_EXTRA_MODULES_PATH'] = './opencv_contrib/modules'
+            cmake.definitions['OPENCV_EXTRA_MODULES_PATH'] = './opencv_contrib-4.2.0/modules'
 
-        cmake.configure(source_folder="opencv")
+        cmake.configure(source_folder="opencv-4.2.0")
         return cmake
 
     def build(self):
